15bf8e10c037dcf11e9c3948e01d8444a7d6a668
MESSANVI Jean-Luc <12415197@crit-xubuntu>
2024-10-11 12:14:05 +0200

    Q1

---
+++ tp2.py
@@ -0,0 +1,16 @@
+def affiche():
+    result = ""
+    for i in range(1, 100):
+        if i % 15 == 0:
+            result += "FrisBee"
+        elif i % 3 == 0:
+            result += "Fizz"
+        elif i % 5 == 0:
+            result += "Buzz"
+        else:
+            result += str(i)
+
+
+    print(result)
+
+affiche()
