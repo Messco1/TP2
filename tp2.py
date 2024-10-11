8c1ecb352c0ac495d32346bc32d261cda1a5b78d
MESSANVI Jean-Luc <12415197@crit-xubuntu>
2024-10-11 12:18:02 +0200

    transformationTp2 en classe

--- tp2.py
+++ tp2.py
@@ -1,16 +1,19 @@
-def affiche():
-    result = ""
-    for i in range(1, 100):
-        if i % 15 == 0:
-            result += "FrisBee"
-        elif i % 3 == 0:
-            result += "Fizz"
-        elif i % 5 == 0:
-            result += "Buzz"
-        else:
-            result += str(i)
+class fizzBuzz:
+
+    def affiche():
+        result = ""
+        for i in range(1, 100):
+            if i % 15 == 0:
+                result += "FrisBee"
+            elif i % 3 == 0:
+                result += "Fizz"
+            elif i % 5 == 0:
+                result += "Buzz"
+            else:
+                result += str(i)
 
 
-    print(result)
+        print(result)
+        return result
 
-affiche()
+
