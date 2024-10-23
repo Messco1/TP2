4d5162826b0d7dc0764af9b1c4adf8c1cd33008c
Jean-Luc MESSANVI <lucmessco@gmail.com>
2024-10-23 17:33:53 +0200

    Ajout du decryptage

--- cryptage.py
+++ cryptage.py
@@ -4,3 +4,10 @@ def crypt(message, pas):
     caracteres = string.ascii_letters + string.punctuation + string.digits + " "
     encrypted_message = ''.join(chr(ord(char) + 1) for char in message if char in caracteres)
     return f"{encrypted_message} {pas}"
+
+def decrypt(message):
+    parts = message.rsplit(" ", 1)
+    encrypted_message = parts[0]
+    pas = int(parts[1])
+    decrypted_message = ''.join(chr(ord(char) - pas) for char in encrypted_message)
+    return decrypted_message
--- test_cryptage.py
+++ test_cryptage.py
@@ -1,9 +1,12 @@
 import unittest
 from cryptage import crypt
+from cryptage import decrypt
+
 
 class TestCryptage(unittest.TestCase):
-    def test_crypt(self):
-        self.assertEqual(crypt("abc", 1), "bcd 1")
+    def test_decrypt(self):
+        encrypted = crypt("abc", 1)
+        self.assertEqual(decrypt(encrypted), "abc")
 
 if __name__ == "__main__":
     unittest.main()
