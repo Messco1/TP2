f4883a38cba043e8dc00a16c77e994d6c9693ddd
Jean-Luc MESSANVI <lucmessco@gmail.com>
2024-10-23 17:22:51 +0200

    Cryptage avec le pas

--- cryptage.py
+++ cryptage.py
@@ -1,6 +1,6 @@
 import string
 
-def crypt(message):
+def crypt(message, pas):
     caracteres = string.ascii_letters + string.punctuation + string.digits + " "
     encrypted_message = ''.join(chr(ord(char) + 1) for char in message if char in caracteres)
-    return encrypted_message
+    return f"{encrypted_message} {pas}"
--- test_cryptage.py
+++ test_cryptage.py
@@ -3,7 +3,7 @@ from cryptage import crypt
 
 class TestCryptage(unittest.TestCase):
     def test_crypt(self):
-        self.assertEqual(crypt("abc"), "bcd")
+        self.assertEqual(crypt("abc", 1), "bcd 1")
 
 if __name__ == "__main__":
     unittest.main()
